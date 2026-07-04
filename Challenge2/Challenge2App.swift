12f45aa85ca9c65a7e2b5970dd453f843ab18aaf
Serene <personalzbx@gmail.com>
2026-07-04 14:22:51 +0800

    Initial Commit

---
+++ Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by Lim Shi Hui Serene     on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge2
+//
+//  Created by Lim Shi Hui Serene     on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
