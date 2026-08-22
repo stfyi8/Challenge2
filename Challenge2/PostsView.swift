4b7c4371e81c69466867bd02e2d9defdf5a11183
Serene <personalzbx@gmail.com>
2026-08-22 14:31:33 +0800

    cleaned up PostsView

--- Challenge2/PostsView.swift
+++ Challenge2/PostsView.swift
@@ -1,39 +1,28 @@
-//
-//  PostsView.swift
-//  SwiftChallenge2
-//
-//  Created by Andika Wahyudi on 25/7/26.
-//
-
 import SwiftUI
 
 struct PostsView: View {
-    let colors: [Color] = [.red, .blue, .green, .orange, .pink]
+    let colors: [Color] = [.red, .blue, .green, .orange, .purple]
     
     @State private var currentIndex: Int = 0
-    @ObservedObject var firestoreManager: FirestoreManager
     @State private var text = ""
-    
+    @State private var counter = 0
     var body: some View {
         VStack(spacing: 20) {
             TabView(selection: $currentIndex) {
-                ForEach(0..<firestoreManager.notes.count, id: \.self) { index in
-                    ZStack{
-                        Rectangle()
-                            .fill(colors[index % colors.count])
-                            .cornerRadius(20)
-                            .shadow(color: .black.opacity(0.15), radius: 8, x: 0, y: 4)
-                            .frame(height: 380)
-                            .padding(.horizontal, 40)
-                            .tag(index)
-                        
-                        Text(firestoreManager.notes[index].text)
-                    }
+                ForEach(0..<counter, id: \.self) { index in
+                    Rectangle()
+                        .fill(colors[index % colors.count])
+                        .cornerRadius(20)
+                        .shadow(color: .black.opacity(0.15), radius: 8, x: 0, y: 4)
+                        .frame(height: 380)
+                        .padding(.horizontal, 40)
+                        .tag(index)
                 }
             }
-            .tabViewStyle(.page(indexDisplayMode: .never))
-            Text("Post \(firestoreManager.notes.isEmpty ? 0 :currentIndex + 1) of \(firestoreManager.notes.count)")
-            
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        Text("Card \(currentIndex + 1) of \(colors.count)")
+
             TextField(
                 "Text",
                 text: $text
@@ -42,32 +31,21 @@ struct PostsView: View {
             
             HStack{
                 Button("Save") {
-                    firestoreManager.addNote(text:text)
-                    text = ""
+                    text=""
+                    counter+=1
                 }
                 .padding(.horizontal,30)
                 
                 Button("Delete") {
-                    guard !firestoreManager.notes.isEmpty else {return} // if got things delete else nill
-                    let noteDelete = firestoreManager.notes[currentIndex] // countes current index(newest text saved)
-                    firestoreManager.deleteNote(note: noteDelete) // deletes currentIndex
+                    counter-=1
                 }
-                
                 .foregroundStyle(.red)
                 .padding(.horizontal,30)
-                // FIXED: This starts the listener when the user opens the screen very IMPT
-                .onAppear {
-                    firestoreManager.getNotes()
-                }
-            }
-            
         }
-        
+        .padding()
     }
 }
 
-
-// used gemni help me debug 😅
 #Preview {
-    PostsView(firestoreManager: FirestoreManager())
+    PostsView()
 }
